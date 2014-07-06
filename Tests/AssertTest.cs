5f5d677e1cc515cda70ee337a28ed727d54f05b0
LazyBui <Lazy.Bui@Gmail.com>
2014-07-06 13:57:15 -0500

    Adding StringAssertions category with the ability to match arbitrary regexen and count matches if desired

--- Tests/AssertTest.cs
+++ Tests/AssertTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test {
@@ -516,6 +517,48 @@ namespace Test {
 
 			Assert.DoesNotThrow(() => Assert.NotUnique(doesNotThrow, v => v.Property));
 			Assert.Throws<AssertionException>(() => Assert.NotUnique(throws, v => v.Property));
+		}
+
+		[TestMethod]
+		public void IsMatch() {
+			Assert.Throws<ArgumentNullException>(() => Assert.IsMatch(null as Regex, string.Empty));
+			Assert.Throws<ArgumentNullException>(() => Assert.IsMatch(new Regex(string.Empty), null));
+
+			Assert.Throws<AssertionException>(() => Assert.IsMatch("abc", "cba"));
+			Assert.Throws<AssertionException>(() => Assert.IsMatch(new Regex("abc"), "cba"));
+			Assert.DoesNotThrow(() => Assert.IsMatch("abc", "abc"));
+			Assert.DoesNotThrow(() => Assert.IsMatch(new Regex("abc"), "abc"));
+		}
+
+		[TestMethod]
+		public void IsNotMatch() {
+			Assert.Throws<ArgumentNullException>(() => Assert.IsNotMatch(null as Regex, string.Empty));
+			Assert.Throws<ArgumentNullException>(() => Assert.IsNotMatch(new Regex(string.Empty), null));
+
+			Assert.DoesNotThrow(() => Assert.IsNotMatch("abc", "cba"));
+			Assert.DoesNotThrow(() => Assert.IsNotMatch(new Regex("abc"), "cba"));
+			Assert.Throws<AssertionException>(() => Assert.IsNotMatch("abc", "abc"));
+			Assert.Throws<AssertionException>(() => Assert.IsNotMatch(new Regex("abc"), "abc"));
+		}
+
+		[TestMethod]
+		public void MatchCount() {
+			Assert.Throws<ArgumentNullException>(() => Assert.MatchCount(null as Regex, string.Empty, 1));
+			Assert.Throws<ArgumentNullException>(() => Assert.MatchCount(new Regex(string.Empty), null, 1));
+			Assert.Throws<ArgumentException>(() => Assert.MatchCount(new Regex(string.Empty), string.Empty, -1));
+
+			Assert.DoesNotThrow(() => Assert.MatchCount(new Regex("abc"), "abcabc", 2));
+			Assert.Throws<AssertionException>(() => Assert.MatchCount(new Regex("abc"), "abcabc", 1));
+			Assert.Throws<AssertionException>(() => Assert.MatchCount(new Regex("abc"), "abcabc", 3));
+			Assert.Throws<AssertionException>(() => Assert.MatchCount(new Regex("abc"), "cba", 1));
+			Assert.Throws<AssertionException>(() => Assert.MatchCount(new Regex("abc"), "cba", 2));
+			Assert.DoesNotThrow(() => Assert.MatchCount(new Regex("abc"), "cba", 0));
+			Assert.DoesNotThrow(() => Assert.MatchCount("abc", "abcabc", 2));
+			Assert.Throws<AssertionException>(() => Assert.MatchCount("abc", "abcabc", 1));
+			Assert.Throws<AssertionException>(() => Assert.MatchCount("abc", "abcabc", 3));
+			Assert.Throws<AssertionException>(() => Assert.MatchCount("abc", "cba", 1));
+			Assert.Throws<AssertionException>(() => Assert.MatchCount("abc", "cba", 2));
+			Assert.DoesNotThrow(() => Assert.MatchCount("abc", "cba", 0));
 		}
 
 		[TestMethod]
