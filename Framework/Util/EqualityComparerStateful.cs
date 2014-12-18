b5bfcaf93096bbe0fcfcb6b45c5193d8a377551c
LazyBui <Lazy.Bui@Gmail.com>
2014-12-18 10:38:53 -0600

    Adding more documentation comments

--- Framework/Util/EqualityComparerStateful.cs
+++ Framework/Util/EqualityComparerStateful.cs
@@ -10,6 +10,12 @@ namespace Test {
 		private Func<TValue, TValue, bool> mComparer = null;
 		private Func<TValue, int> mHasher = null;
 
+		/// <summary>
+		/// Initializes a new instance of the Test.EqualityComparerStateful&lt;TValue&gt; class with a specified comparer delegate and optional hasher delegate.
+		/// If the hasher is not specified, the hash of all objects is assumed to be 0.
+		/// </summary>
+		/// <param name="pComparer">The comparer that determines whether two objects are equal.</param>
+		/// <param name="pHasher">The hash function for a particular object.</param>
 		public EqualityComparerStateful(Func<TValue, TValue, bool> pComparer, Func<TValue, int> pHasher = null) {
 			if (pComparer == null) throw new ArgumentNullException("pComparer");
 			if (pHasher == null) pHasher = v => 0;
@@ -18,10 +24,21 @@ namespace Test {
 			mHasher = pHasher;
 		}
 
+		/// <summary>
+		/// Determines whether the specified objects are equal.
+		/// </summary>
+		/// <param name="x">The first object of type TValue to compare.</param>
+		/// <param name="y">The second object of type TValue to compare.</param>
+		/// <returns>true if the specified objects are equal; otherwise, false.</returns>
 		public bool Equals(TValue x, TValue y) {
 			return mComparer(x, y);
 		}
 
+		/// <summary>
+		/// Returns a hash code for the specified object.
+		/// </summary>
+		/// <param name="obj">The TValue for which a hash code is to be returned.</param>
+		/// <returns>A hash code for the specified object.</returns>
 		public int GetHashCode(TValue obj) {
 			return mHasher(obj);
 		}
