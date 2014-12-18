95cb3ee75e1611b5b990100ca2b38d49a3b21a8b
LazyBui <Lazy.Bui@Gmail.com>
2014-12-18 10:28:28 -0600

    Some documentation

--- Framework/Util/WindowsCommandLineArgumentEscaper.cs
+++ Framework/Util/WindowsCommandLineArgumentEscaper.cs
@@ -4,8 +4,17 @@ using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Test {
+	/// <summary>
+	/// Represents a conversion from a collection of objects to a Windows command line argument string.
+	/// </summary>
 	internal sealed class WindowsCommandLineArgumentEscaper : ICommandLineArgumentEscaper {
 		private const string Quote = @"""";
+
+		/// <summary>
+		/// Escapes a collection of objects to a usable Windows command line argument string.
+		/// </summary>
+		/// <param name="pRawArguments">A collection of objects to be used to compose a Windows command line argument string.</param>
+		/// <returns>A usable Windows command line argument string.</returns>
 		public string Escape(IEnumerable<object> pRawArguments) {
 			return string.Join(" ",
 				pRawArguments.Select(a => EscapeArgument(a.ToString())));
